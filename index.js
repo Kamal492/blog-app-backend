9a69d3144819c0ea546e03612500d808f337c60d
Kamal492 <58765013+Kamal492@users.noreply.github.com>
2024-01-08 10:08:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ app.use(cookieParser());
 
 
 const allowCrossDomain = function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', "http://localhost:3000");
+  res.header('Access-Control-Allow-Origin', "https://blog-app-frontend-vyxk.onrender.com");
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
