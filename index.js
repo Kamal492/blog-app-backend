db795b318ce2fc62bc1469f1e5151a91c39e1f7d
Kamal492 <58765013+Kamal492@users.noreply.github.com>
2024-01-18 00:45:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -51,16 +51,8 @@ app.use("/blog/subscribe", SubscriberRoute)
 
 app.use(cookieParser());
 
+app.use(cors());
 
-
-const allowCrossDomain = function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'https://blog-diaries.netlify.app');
-  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  res.header('Access-Control-Allow-Credentials', true); // If you are using credentials, set this header
-  next();
-}
-app.use(allowCrossDomain);
 app.use((req, res, next) => {
   res.setHeader('Cache-Control', 'no-store');
   next();
