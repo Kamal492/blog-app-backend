58136ff279f49a7c4514665c3e613c45c21ddea2
Kamal492 <58765013+Kamal492@users.noreply.github.com>
2024-01-18 00:27:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ app.use(cookieParser());
 
 
 const allowCrossDomain = function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', "https://blog-diaries.netlify.app/");
+  res.header('Access-Control-Allow-Origin', "*");
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
